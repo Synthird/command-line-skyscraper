ea47ed02a52f3540186f36975945fc9ca0e483f0
Synthird <105394020+Synthird@users.noreply.github.com>
2024-10-29 13:42:15 -0700

    Made the word only between the words enter and integers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 print("How many floors do you want in your skyscraper?")
 
-floors = int(input("Floors (Please only enter integers): "))
+floors = int(input("Floors (Please enter only integers): "))
 
 for _ in range(3):
     print("   .")

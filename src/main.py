ecd46fedadd48065f34f5497ba79c04ad5804461
Synthird <105394020+Synthird@users.noreply.github.com>
2024-10-29 13:31:04 -0700

    Specify only ints when asking for floors

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-print("How many floors do you want in your skyscraper?")
+print("How many floors should be in your skyscraper?")
 
-floors = int(input("Floors: "))
+floors = int(input("Floors (Please enter only an integer): "))
 
 for _ in range(3):
     print("   .")

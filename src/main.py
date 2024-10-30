e84f03a30682db636cf226ebc4b53b0ebada151e
Synthird <105394020+Synthird@users.noreply.github.com>
2024-10-30 14:43:42 -0700

    Added static typing and ask the user how tall they want their lightning rod to be

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-def print_multiple_times(message, iterations):
+def ask_question(question, prompt) -> str:
+    print(f"--- {question} ---")
+    print("(Please don't enter decimals or units)")
+    answer: int = int(input(f"{prompt}: "))
+    return answer
+
+
+def print_multiple_times(message, iterations) -> None:
     for _ in range(iterations):
         print(message)
 
 
-print("How many floors do you want in your skyscraper?")
+# Floors
+floors: int = ask_question(
+    "How many floors do you want in your skyscraper?", "Floors")
 
-floors = int(input("Floors (Please enter only an Integer): "))
+# Lightning rod
+rod_dots: int = ask_question(
+    "How tall do you want your lightning rod to be?", "Number of dots for the rod")
 
-print_multiple_times("   .", 3)
+print_multiple_times("   .", rod_dots)
 
 print_multiple_times(".......", floors)

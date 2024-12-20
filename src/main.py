d195ca9bf2a1ef0371478f17024823b0a3d94102
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-19 21:42:46 -0800

    Replace spaces with tabs when code is indented

--- src/main.py
+++ src/main.py
@@ -1,33 +1,33 @@
 def ask_question(question: str, prompt: str) -> int:
-    print(f"--- {question}? ---")
-    try:
-        number: int = int(input(f"{prompt}: ").replace(" ", ""))
+	print(f"--- {question}? ---")
+	try:
+		number: int = int(input(f"{prompt}: ").replace(" ", ""))
 
-        if number > -1:
-            return number
-        else:
-            raise ValueError
-    except:
-        print("!!! Cannot print a skyscraper! !!!")
-        print("Probably because you entered:")
-        print("a) Decimals")
-        print("b) Letters")
-        print("c) Symbols")
-        print("d) A negative number")
-        print("e) You exited the program")
-        raise SystemExit
+		if number > -1:
+			return number
+		else:
+			raise ValueError
+	except:
+		print("!!! Cannot print a skyscraper! !!!")
+		print("Probably because you entered:")
+		print("a) Decimals")
+		print("b) Letters")
+		print("c) Symbols")
+		print("d) A negative number")
+		print("e) You exited the program")
+		raise SystemExit
 
 
 def print_multiple_times(message: str, iterations: int) -> None:
-    for _ in range(iterations):
-        print(message)
+	for _ in range(iterations):
+		print(message)
 
 
 floors: int = ask_question(
-    "How many floors do you want in your skyscraper", "Floors")
+	"How many floors do you want in your skyscraper", "Floors")
 
 rod_dots: int = ask_question(
-    "How tall do you want your lightning rod to be", "# of dots for the rod")
+	"How tall do you want your lightning rod to be", "# of dots for the rod")
 
 print_multiple_times("   .", rod_dots)
 print_multiple_times(".......", floors)
